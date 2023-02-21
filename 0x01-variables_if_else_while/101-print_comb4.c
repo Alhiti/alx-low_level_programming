ccc0abce559e305fb21c45f703dec59cc8177700
Alhiti <ali.alhiti05@gmail.com>
2023-02-20 19:07:19 -0800

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,11 +8,12 @@ int main(void)
 {
 
 	int n, m, l;
-	
+
+
 
 	for (n = 48; n < 58; n++)
 	{
-		
+
 		for (m = 49; m < 58; m++)
 		{
 
@@ -21,33 +22,26 @@ int main(void)
 
 				if (l > m && m > n)
 				{
-						putchar(n);
 
-						putchar(m);				
+					putchar(n);
 
-						putchar(l);
+					putchar(m);
 
-						if (n != 55 || m != 56)
-						{
-						
-						
+					putchar(l);
 
-							putchar(',');
+					if (n != 55 || m != 56)
+					{
 
-							putchar(' ');
+						putchar(',');
 
-						}
-
+						putchar(' ');
 					}
-
 				}
-
 			}
-
 		}
+	}
 
-		putchar('\n');
+	putchar('\n');
 
-		return (0);
-
+	return (0);
 }
